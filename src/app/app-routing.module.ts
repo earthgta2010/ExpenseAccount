824d19b1114ff61bc1808c821b4bf4e1baf4e4d3
earthgta2010 <earthgta2010@gmail.com>
2019-07-01 15:35:46 +0700

    test log

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'Upload'},
+  {path: '', pathMatch: 'full', redirectTo: 'Upload'}, //go to upload page
 ];
 
 @NgModule({
